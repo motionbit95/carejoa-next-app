1a2c18ffd51c60286fe11823dcfc61b1e454ab0f
motionbit95 <motionbit.dev@gmail.com>
2025-02-07 11:20:16 +0900

    AppLayout 컴포넌트에 헤더 및 푸터 표시 여부와 제목을 위한 props 추가 및 PropTypes 설정

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types"; // PropTypes 가져오기
 import { ArrowLeftOutlined } from "@ant-design/icons";
-import { Layout, Menu, Typography } from "antd";
+import { Layout, Typography } from "antd";
 import Link from "next/link";
 
 const { Header, Content, Footer } = Layout;
 
-const AppLayout = ({ children, ...props }) => (
+const AppLayout = ({
+  children,
+  hasHeader = true,
+  hasFooter = false,
+  title,
+}) => (
   <Layout style={{ minHeight: "100vh" }}>
-    <Header style={{ zIndex: 1, width: "100%", paddingInline: 16 }}>
-      <ArrowLeftOutlined />
-      <Typography.Title level={3}></Typography.Title>
-    </Header>
+    {hasHeader && (
+      <Header
+        style={{
+          zIndex: 1,
+          width: "100%",
+          paddingInline: 16,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Link href="/" passHref>
+          <ArrowLeftOutlined
+            style={{ fontSize: 18, marginRight: 16, cursor: "pointer" }}
+          />
+        </Link>
+        <Typography.Title level={3} style={{ margin: 0 }}>
+          {title}
+        </Typography.Title>
+      </Header>
+    )}
     <Content style={{ padding: "20px" }}>{children}</Content>
-    {props.hasFooter && (
+    {hasFooter && (
       <Footer style={{ textAlign: "center" }}>
         My App ©2025 Created with Ant Design
       </Footer>
     )}
   </Layout>
 );
+
+// PropTypes 설정
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired, // JSX 요소를 자식으로 받을 때
+  hasHeader: PropTypes.bool, // Boolean 타입
+  hasFooter: PropTypes.bool, // Boolean 타입
+  title: PropTypes.string, // 문자열 타입
+};
+
+// 기본값 설정
+AppLayout.defaultProps = {
+  hasHeader: true,
+  hasFooter: false,
+  title: "",
+};
 
 export default AppLayout;
